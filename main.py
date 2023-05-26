c198e9fcdbb379638d1dea83d58421af66ded20a
Chuchis02 <134682054+Chuchis02@users.noreply.github.com>
2023-05-26 02:59:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
-
+from pydantic import BaseMode1
 
 app=FastAPI()
 
-
 @app.get("/")
 def index(): 
-    return "Hola :)"
+        return "Hola :)"
+
 @app.get("/Pokemon/{num}")
 def pokemon(num):
-    pokemons={
-       "1":"Bulbasur",
-       "2":"Ivysaur",
-       "3":"Venasur",
-       "4":"Charmander",
+        pokemons={
+        "1":"Bulbasur",
+        "2":"Ivysaur",
+        "3":"Venasur",
+        "4":"Charmander",
         
        }
     return pokemons{num}
