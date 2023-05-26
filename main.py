7d310ef013c71be4b515d588e7d5fe7492d6cd5a
Chuchis02 <134682054+Chuchis02@users.noreply.github.com>
2023-05-26 10:55:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def conversorCaf(C):
                 C=float(C)
                 TF=C*(9/5) + 32
                 return f"La temperatura es de {TF} grados farenheit"
-            except: 
+       except: 
                      return "Entrada invalida"
         
 @app.get("/RevisarEdad/{E1}/{E2}")
