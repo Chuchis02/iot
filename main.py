f11a20c0720959681c752234eedfdcffc2d1db2f
Chuchis02 <134682054+Chuchis02@users.noreply.github.com>
2023-05-26 03:32:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,23 @@ def conversorCaf(C):
                 return f"La temperatura es de {TF} grados farenheit"
             except: 
                      return "Entrada invalida"
+        
+@app.get("/RevisarEdad/{E1}/{E2}")
+def revisar_edades(E1,E2):
+    E1=int(E1)
+    E2=int(E2)
+    if E1>E2+30:
+        return "Podrias ser su Abuelo"
+    elif E1>E2+15:
+        return "Podrias ser su padre"
+    elif E1>E2:
+        return "eres mayor"
+    elif E2>E1+30:
+        return "podria ser tu abuelo"
+    elif E2>E1+15:
+        return "Podria ser tu padre"
+    elif E2>E1:
+        return "Es mayor que tu"
+    else:
+        return "tienen la misma edad"
+    
